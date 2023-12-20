bd55520116a6dc6b420d59979aab4a2c38d4af58
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2023-12-20 16:08:18 +0100

    I was applying gmres wrong whole the time

--- code_julia/MG_preconditioner.jl
+++ code_julia/MG_preconditioner.jl
@@ -1,4 +1,4 @@
-using Plots, SparseArrays, LinearAlgebra, IterativeSolvers, Revise
+using Plots, SparseArrays, LinearAlgebra, Revise
 include("nonuniformMultiGrid.jl")
 
 struct myPreconditioner
@@ -16,17 +16,47 @@ function LinearAlgebra.ldiv!(P::myPreconditioner, x)
 end
 
 function LinearAlgebra.:\(P::myPreconditioner, x)
-    return P.pre_con(x)
+    return P.precon(x)
 end
 
-function nonuniform_preconditioner(; grid, H, f, nu1, nu2)
+function LinearAlgebra.:mul!(y, P::myPreconditioner, x)
+    y .= P.precon(x)
+    return y
+end
+
+function nonuniform_preconditioner(; grid, H, nu1, nu2)
     pres(x) = nonUniformVcycle2D(
         grid=grid,
         A=H,
-        f=f,
-        u=x,
+        f=x,
+        u=zero(x),
         nu1=nu1,
         nu2=nu2,
         recursion_depth=50)
     return myPreconditioner(pres)
+end
+
+function gmres_callbackX!(X, solver)
+    z = solver.z
+    k = solver.inner_iter
+    nr = sum(1:k)
+    V = solver.V
+    R = solver.R
+    y = copy(z)
+
+    # Solve Rk * yk = zk
+    for i = k:-1:1
+        pos = nr + i - k
+        for j = k:-1:i+1
+            y[i] = y[i] - R[pos] * y[j]
+            pos = pos - j + 1
+        end
+        y[i] = y[i] / R[pos]
+    end
+
+    # xk = Vk * yk
+    xk = sum(V[i] * y[i] for i = 1:k)
+    push!(X, xk)
+
+    return false  # We don't want to add new stopping conditions
 end
